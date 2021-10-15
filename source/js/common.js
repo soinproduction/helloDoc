a8ad8956744d91374598eca976f1a8d5a01bbdb2
admin <soinproduction@gmail.com>
2021-10-15 08:38:45 +0300

    add beta desktop version

--- source/js/common.js
+++ source/js/common.js
@@ -5,14 +5,37 @@ const anchors = document.querySelectorAll('a[href*="#"]');
 let docBtn = document.querySelector('.doc-btn');
 let caseBtn = document.querySelector('.case-btn');
 
-window.onscroll = function() {fixedHeader()}
-function fixedHeader() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    header.classList.add('fixed');
-  } else {
-    header.classList.remove('fixed');
+document.addEventListener("DOMContentLoaded", function(event) {
+  window.onscroll = function() {fixedHeader()}
+
+  function fixedHeader() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      header.classList.add('fixed');
+    } else {
+      header.classList.remove('fixed');
+    }
   }
-}
+
+  for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault()
+      const blockID = anchor.getAttribute('href').substr(1)
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      })
+    })
+  }
+
+  AOS.init();
+
+  if (body.classList.contains('light')) {
+    docBtn.classList.add('active');
+  } else {
+    docBtn.classList.remove('active');
+    caseBtn.classList.add('active');
+  }
+});
 
 const accordeon = {
   CLASS: 'accordion',
@@ -52,28 +75,9 @@ for (const accordeon of acc) {
 };
 
 
-for (let anchor of anchors) {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault()
-    const blockID = anchor.getAttribute('href').substr(1)
-    document.getElementById(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    })
-  })
-}
 
 
-// if ( window.location.pathname === '/index_2.html') {
-//   body.classList.add('light')
-// } else {
-//   body.classList.remove('light');
-// }
 
 
-if (body.classList.contains('light')) {
-  docBtn.classList.add('active');
-} else {
-  docBtn.classList.remove('active');
-  caseBtn.classList.add('active');
-}
+
+
