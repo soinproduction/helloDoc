dfe3110f154fad6dc148b9eb6dd1fe52d3ae1b35
admin <soinproduction@gmail.com>
2021-10-15 16:48:59 +0300

    fix bugs

--- source/js/common.js
+++ source/js/common.js
@@ -1,9 +1,10 @@
-let header = document.querySelector('header');
-let body = document.body;
+const header = document.querySelector('header');
+const body = document.body;
 const anchors = document.querySelectorAll('a[href*="#"]');
-
-let docBtn = document.querySelector('.doc-btn');
-let caseBtn = document.querySelector('.case-btn');
+const docBtn = document.querySelector('.doc-btn');
+const caseBtn = document.querySelector('.case-btn');
+const btn = document.querySelector('.doc-btn');
+const btnCase = document.querySelector('.case-btn');
 
 document.addEventListener("DOMContentLoaded", function(event) {
   window.onscroll = function() {fixedHeader()}
@@ -29,12 +30,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   AOS.init();
 
-  if (body.classList.contains('light')) {
-    docBtn.classList.add('active');
-  } else {
-    docBtn.classList.remove('active');
-    caseBtn.classList.add('active');
+
+
+  btn.addEventListener('click', toggleHeader);
+  btnCase.addEventListener('click', removeHeader);
+
+  function toggleHeader() {
+    header.classList.add('light');
   }
+
+  function removeHeader() {
+    header.classList.remove('light');
+  }
+
 });
 
 const accordeon = {
@@ -74,6 +82,27 @@ for (const accordeon of acc) {
   });
 };
 
+
+function openPage(evt, pageName) {
+  // Объявить все переменные
+  var i, tabcontent, tablinks;
+
+  // Получить все элементы с помощью class="tabcontent" и спрятать их
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Получить все элементы с помощью class="tablinks" и удалить class "active"
+  tablinks = document.getElementsByClassName("nav-buttons__button");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Показать текущую вкладку и добавить "active" класс для кнопки, которая открыла вкладку
+  document.getElementById(pageName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
 
 
 
